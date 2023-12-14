54bc9811e8d40c1ca62e8fbb34b66fcc9eb9864e
Jidraf <117856834+RAPHJID@users.noreply.github.com>
2023-12-14 10:34:06 +0300

    Add files via upload

---
+++ HospitalDbContext.cs
@@ -0,0 +1,47 @@
+﻿using HospitalManagementAssessment.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class HospitalDbContext : DbContext
+{
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DiJ; Database=MyHospital; Encrypt=True; TrustServerCertificate=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+     //patient
+        modelBuilder.Entity<Patient>()
+            .HasKey(p => p.PatientId);
+
+        modelBuilder.Entity<Patient>()
+            .HasOne(p => p.Room)
+            .WithMany(r => r.Patients)
+            .HasForeignKey(p => p.RoomId);
+        //Doctor
+        modelBuilder.Entity<Doctor>()
+            .HasKey(d => d.DoctorId);
+        //Appointment
+        modelBuilder.Entity<Appointment>()
+            .HasKey(a => a.AppointmentId);
+
+        modelBuilder.Entity<Appointment>()
+            .HasOne(a => a.Patient)
+            .WithMany(p => p.Appointments)
+            .HasForeignKey(a => a.PatientId);
+        modelBuilder.Entity<Appointment>()
+            .HasOne(a => a.Doctor)
+            .WithMany(d => d.Appointments)
+            .HasForeignKey(a => a.DoctorId);
+        //Room
+        modelBuilder.Entity<Room>()
+            .HasKey(r => r.RoomId);
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+using HospitalManagementAssessment.Models;
+
+{
+    static void Main()
+    {
+        using (var context = new HospitalDbContext())
+        {
+            // Create
+            var newPatient = new Patient { FullName = "John Doe" };
+            context.Patients.Add(newPatient);
+            context.SaveChanges();
+
+            // Read
+            var allPatients = context.Patients.ToList();
+            foreach (var patient in allPatients)
+            {
+                Console.WriteLine($"Patient ID: {patient.PatientId}, Name: {patient.FullName}");
+            }
+
+            // Update
+            var patientToUpdate = context.Patients.FirstOrDefault(p => p.FullName == "Jid doe");
+            if (patientToUpdate != null)
+            {
+                patientToUpdate.FullName = "Jid";
+                context.SaveChanges();
+            }
+
+            // Delete
+            var patientToDelete = context.Patients.FirstOrDefault(p => p.FullName == "Jid doe");
+            if (patientToDelete != null)
+            {
+                context.Patients.Remove(patientToDelete);
+                context.SaveChanges();
+            }
+        }
+    }
+}
